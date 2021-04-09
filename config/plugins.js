56d0a9820806c0c0b54d2d4836b10381430c7cf7
ManoRanjana de Silva <manoranjana@arimaclanka.com>
2021-04-09 14:42:56 +0530

    plugin update

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,15 @@
 module.exports = ({ env }) => ({
     upload: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          Bucket: env('AWS_BUCKET'),
+        provider: 'aws-s3',
+        providerOptions: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            region: env('AWS_REGION'),
+            params: {
+                Bucket: env('AWS_BUCKET'),
+            },
+            storageUrl: env('STORAGE_ENDPOINT'),
+            cdnUrl: env('CDN_ENDPOINT')
         },
-      },
     },
-  });
+});
